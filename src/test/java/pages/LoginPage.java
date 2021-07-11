156047929758c2fd3dca435b726d0d4f159d71f0
David Nascimento <halison700@gmail.como>
2021-07-11 08:43:42 -0300

    Refatorado classe de LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,9 +2,7 @@ package pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import tests.LoginTests;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 
